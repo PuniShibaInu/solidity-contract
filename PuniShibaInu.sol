dae7ea46c19caea0f1a9bd765c986f4288df5f71
PuniShibaInu <84247512+PuniShibaInu@users.noreply.github.com>
2021-05-17 11:14:59 +0300

    Update PuniShibaInu.sol

--- PuniShibaInu.sol
+++ PuniShibaInu.sol
@@ -747,7 +747,7 @@ contract PuniShibaInu is Context, IERC20, Ownable {
     constructor () public {
         _rOwned[_msgSender()] = _rTotal;
         
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
@@ -1151,5 +1151,6 @@ contract PuniShibaInu is Context, IERC20, Ownable {
         _reflectFee(rFee, tFee);
         emit Transfer(sender, recipient, tTransferAmount);
     }
+    
 }
 
